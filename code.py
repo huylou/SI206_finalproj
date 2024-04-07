74f828beb00b24834c8d9cb29e628f5b146c6882
JONATHAN LOU <huylou@umich.edu>
2024-04-06 22:19:53 -0400

    we did something

--- code.py
+++ code.py
@@ -1,1 +1,13 @@
+import unittest
+import sqlite3
+import requests
+import json
+import os
+import re
+import matplotlib.pyplot as plt
+
+#import carbon intensity API
+headers = {'Accept': 'application/json'}
+r = requests.get('https://api.carbonintensity.org.uk/intensity/date', params={}, headers = headers)
+
 
