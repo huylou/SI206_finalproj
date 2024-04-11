c227628c0c655b395befe1c873f391a78a1aae13
JONATHAN LOU <huylou@umich.edu>
2024-04-10 22:10:58 -0400

    hehe

--- carbon_intensity.py
+++ carbon_intensity.py
@@ -3,11 +3,10 @@ import sqlite3
 import requests
 import json
 import os
-import re
-import matplotlib.pyplot as plt
 
 #import carbon intensity API
 headers = {'Accept': 'application/json'}
-r = requests.get('https://api.carbonintensity.org.uk/intensity/date', params={}, headers = headers)
+r = requests.get('https://api.carbonintensity.org.uk/regional/regionid/13', params={}, headers = headers).json()
+print(r)
 
 
