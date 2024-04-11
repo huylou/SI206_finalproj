b1db2cf359fa324c8dbfe8e4bd75f992aa1d885e
JONATHAN LOU <huylou@umich.edu>
2024-04-10 22:59:22 -0400

    electricity costs per kWh

--- electricity_costs.py
+++ electricity_costs.py
@@ -1,1 +1,12 @@
+import unittest
+import sqlite3
+import requests
+import json
+import os
 
+dno = 'dno=12'
+voltage = 'voltage=LV'
+start = 'start=09-04-2024'
+end = 'end=10-04-2024'
+r = requests.get(f'https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?{dno}&{voltage}&{start}&{end}').json()
+print(r)
