dd3184767707a012bc76761190d2888f50330584
Jessica Tran <jktran@umich.edu>
2024-04-13 20:52:47 -0400

    fixed db to store 24 lines at a time

--- carbon_intensity.py
+++ carbon_intensity.py
@@ -31,25 +31,41 @@ def create_table(r, cur, conn):
     '''
     Arguments: takes the json output from the API request
     '''
-    for i in range(len(r['data']['data'])):
-        # cur.execute("DROP TABLE IF EXISTS Carbon_Intensity_Data")
-        cur.execute("CREATE TABLE IF NOT EXISTS Carbon_Intensity_Data (timestamp TEXT PRIMARY KEY, date TEXT, time TEXT, intensity_forecast TEXT)")
 
-    #initializing variables that will go into database
+    # cur.execute("DROP TABLE IF EXISTS Carbon_Intensity_Data")
+    cur.execute("CREATE TABLE IF NOT EXISTS Carbon_Intensity_Data (timestamp TEXT PRIMARY KEY, date TEXT, time TEXT, intensity_forecast TEXT)")
+
+    #find existing timestamps in database
+    count = 0
+    cur.execute("SELECT timestamp FROM Carbon_Intensity_Data") #returns tuple of timestamps
+    current = list(cur.fetchall())
+    # print(current)
+    existing_timestamps = []
+    for tup in current:
+        for t in tup: 
+            existing_timestamps.append(t)
+
+    #limit retrieval to 24 lines at a time
     for interval in r['data']['data']: 
-        # print(interval)
-        start_time = interval['from'][-6:-1] 
-        # print(start_time)
-        end_time = interval['to'][-6:-1]
-        # print(end_time)
-        date = interval['from'][8:10] + '-' + interval['from'][5:7] + '-' + interval['from'][:4]
-        timestamp = interval['from'][-6:-1] + ' ' + interval['from'][8:10] + '-' + interval['from'][5:7] + '-' + interval['from'][:4]
-        # print(timestamp)
-        intensity_forecast = interval['intensity']['forecast']
-        # print(intensity_forecast)
-        cur.execute("INSERT OR IGNORE INTO Carbon_Intensity_Data (timestamp, date, time, intensity_forecast) VALUES (?,?,?,?)", (timestamp, date, start_time, intensity_forecast))
-
+        if count == 24: 
+            break
+        else: 
+            #initializing variables that will go into database
+            # print(interval)
+            start_time = interval['from'][-6:-1] 
+            # print(start_time)
+            intensity_forecast = interval['intensity']['forecast']
+            # print(intensity_forecast)
+            date = interval['from'][8:10] + '-' + interval['from'][5:7] + '-' + interval['from'][:4]
+            timestamp = interval['from'][-6:-1] + ' ' + interval['from'][8:10] + '-' + interval['from'][5:7] + '-' + interval['from'][:4]
+            # print(timestamp)
+            if timestamp in existing_timestamps: 
+                continue
+            else:
+                cur.execute("INSERT OR IGNORE INTO Carbon_Intensity_Data (timestamp, date, time, intensity_forecast) VALUES (?,?,?,?)", (timestamp, date, start_time, intensity_forecast))
+                count += 1 # count only increases IF not in database yet
     conn.commit()
+
 
 def main():
     r = api_request('2024-04-09')
