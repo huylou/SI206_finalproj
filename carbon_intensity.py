1c9b7854558b80999b3434eb69b69b8bf1c8462d
JONATHAN LOU <huylou@umich.edu>
2024-04-24 00:37:42 -0400

    created function for generation mix

--- carbon_intensity.py
+++ carbon_intensity.py
@@ -36,17 +36,16 @@ def api_request(date, regionid):
     r = requests.get(f'https://api.carbonintensity.org.uk/regional/intensity/{from_}/{to}/regionid/{regionid}', params={}, headers = headers).json()
     return r
 
-def create_table(r, cur, conn):
+def create_carbon_intensity_table(r, cur, conn):
     '''
     Arguments: takes the json output from the API request
     '''
-
     # cur.execute("DROP TABLE IF EXISTS Carbon_Intensity_Data")
-    cur.execute("CREATE TABLE IF NOT EXISTS Carbon_Intensity_Data (timestamp TEXT PRIMARY KEY, date TEXT, time TEXT, intensity_forecast INTEGER)")
+    cur.execute(f"CREATE TABLE IF NOT EXISTS Carbon_Intensity_Data_UKPNLondon (timestamp TEXT PRIMARY KEY, date TEXT, time TEXT, intensity_forecast INTEGER)")
 
     #find existing timestamps in database
     count = 0
-    cur.execute("SELECT timestamp FROM Carbon_Intensity_Data") #returns tuple of timestamps
+    cur.execute("SELECT timestamp FROM Carbon_Intensity_Data_UKPNLondon") #returns tuple of timestamps
     current = list(cur.fetchall())
     # print(current)
     existing_timestamps = []
@@ -71,7 +70,7 @@ def create_table(r, cur, conn):
             if timestamp in existing_timestamps: 
                 continue
             else:
-                cur.execute("INSERT OR IGNORE INTO Carbon_Intensity_Data (timestamp, date, time, intensity_forecast) VALUES (?,?,?,?)", (timestamp, date, start_time, intensity_forecast))
+                cur.execute("INSERT OR IGNORE INTO Carbon_Intensity_Data_UKPNLondon (timestamp, date, time, intensity_forecast) VALUES (?,?,?,?)", (timestamp, date, start_time, intensity_forecast))
                 count += 1 # count only increases IF not in database yet
     conn.commit()
 
@@ -84,7 +83,7 @@ def calculate_average_intensity_forecast(cur):
     OUTPUT:
         List of Tuples (Timestamp, Average Intensity Forecast Value)'''
     
-    cur.execute("SELECT time, intensity_forecast FROM Carbon_Intensity_Data")
+    cur.execute("SELECT time, intensity_forecast FROM Carbon_Intensity_Data_UKPNLondon")
     d = {}
     lst = list(cur.fetchall())
     avg_list = []
@@ -105,13 +104,61 @@ def calculate_average_intensity_forecast(cur):
     
     return avg_list
 
+def create_generationmix_database(datadict, cur, conn):
+
+    d = {}
+    for interval in datadict['data']['data']:
+        start_time = interval['from'][-6:-1] 
+        date = interval['from'][8:10] + '-' + interval['from'][5:7] + '-' + interval['from'][:4]
+        timestamp = interval['from'][-6:-1] + ' ' + interval['from'][8:10] + '-' + interval['from'][5:7] + '-' + interval['from'][:4]
+        d[timestamp] = {'start_time': start_time, 'date': date}
+        generationmix_dict = interval['generationmix']
+        for mix in generationmix_dict:
+            fuel = mix['fuel']
+            percentage = mix['perc']
+            d[timestamp][fuel] = percentage
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS Generation_Mix_UKPNLondon 
+                (Timestamp TEXT PRIMARY KEY, 
+                Date TEXT, 
+                Time TEXT, 
+                Gas INTEGER, 
+                Coal INTEGER, 
+                Biomass INTEGER, 
+                Nuclear INTEGER,
+                Hydro INTEGER,
+                Wind INTEGER,
+                Solar INTEGER,
+                Imports INTEGER,
+                Other INTEGER)''')
+    
+    cur.execute("SELECT Timestamp FROM Generation_Mix_UKPNLondon")
+    existing = []
+    retrieved = list(cur.fetchall())
+    for keytup in retrieved:
+        existing.append(keytup[0])
+
+    # Insert data into database with the limit of 24 items or less
+    loop_count = 0  
+    for stamp, dct in d.items():
+        cur.execute('''INSERT OR IGNORE INTO Generation_Mix_UKPNLondon (Timestamp, Date, Time, Gas, Coal, Biomass, Nuclear, Hydro, Wind, Solar, Imports, Other) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)''', (stamp, dct['date'], dct['start_time'], dct['gas'], dct['coal'], dct['biomass'], dct['nuclear'], dct['hydro'], dct['wind'], dct['solar'], dct['imports'], dct['other']))
+        if stamp in existing:
+            continue
+        else:
+            loop_count += 1
+
+        if loop_count == 24:
+            break
+
+    conn.commit()
 
 def main():
     r = api_request('2024-04-09', 13)
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path + "/" + 'carbon_intensity.db')
     cur = conn.cursor()
-    create_table(r, cur, conn)
+    create_carbon_intensity_table(r, cur, conn)
+    create_generationmix_database(r, cur, conn)
     avg_inten_list = calculate_average_intensity_forecast(cur)
     
 if __name__ == "__main__":
